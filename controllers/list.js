12b8f8a3082e6364d5dcb4dbf3e883da0f7cf6c4
异名者 <1398947887>
2019-07-12 04:55:06 +0800

    Updated

--- controllers/list.js
+++ controllers/list.js
@@ -1,7 +1,7 @@
 var query = require('myLibs/mysql');
 
 exports.show = function (req, res) {
-    query("SELECT `name` FROM `celebrity_table`", [1], function(err, results, fields){
+    query("SELECT `name` FROM `celebrity_table`", function(err, results, fields){
         if(err){
             res.status(500).send('database error!');
         }else{
